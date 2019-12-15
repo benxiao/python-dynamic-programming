2875934165d12dd3712a678d1e404767fc8e6120
ranxiao <benji.xiao@gmail.com>
2019-12-15 13:07:18 +1100

    clean up vanilla lsc

--- lsc.py
+++ lsc.py
@@ -7,11 +7,17 @@ def lsc(s0, s1):
             else:
                 cache[i+1][j+1] = max(
                     cache[i][j+1],
-                    cache[i+1][j],
-                )
+                    cache[i+1][j])
 
-    print(cache)
+    return cache[len(s0)][len(s1)]
 
 
-if __name__ == '__main__':
-    lsc("AGGTAB", "GXTXAYB")
+# if __name__ == '__main__':
+#     # perfect usecase for messy data
+#     print(lsc("trucson le", "truc le"))
+#     print(str_match("trucson le", "truc le"))
+#
+#     # print(lsc("ahmed", "ahamed"))
+#     # print(len("ummuzahira kamaldeen"),
+#     #       len("ummuzahira hanifamohamedkamaldeen"),
+#     #       lsc("ummuzahira kamaldeen", "ummuzahira hanifamohamedkamaldeen"))
