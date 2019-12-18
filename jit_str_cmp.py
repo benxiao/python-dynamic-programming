7b907ca8875c556b2e880e1213ce65a244151a58
ranxiao <benji.xiao@gmail.com>
2019-12-19 10:57:11 +1100

    add a modified version of edit-distance that optimise for name matching

--- jit_str_cmp.py
+++ jit_str_cmp.py
@@ -2,6 +2,7 @@ import numba as nb
 from numba import njit
 import numpy as np
 
+
 NumbaBytesType = nb.typeof("".encode())
 
 
@@ -20,11 +21,27 @@ def jit_edit_dist(b0: bytes, b1: bytes) -> nb.int32:
             if c0 == c1:
                 cache[i + 1, j + 1] = cache[i, j]
             else:
-                cache[i + 1, j + 1] = 1 + min(
+                penalty = 2 if (i < 2 or j < 2) else 1
+                cache[i + 1, j + 1] = penalty + min(
                     cache[i, j + 1],  # insert
                     cache[i + 1, j],  # delete
                     cache[i, j]  # replace
                 )
+
+                # adjacent character transportation
+                if b0[i] == b1[j-1] and b0[i-1] == b1[j]:
+                    cache[i+1, j+1] = min(
+                        cache[i-1, i-1] + 1,
+                        cache[i+1, j+1]
+                    )
+
+                # two repeats characters counts
+                if i > 0 and j > 0 and b0[i] == b0[i-1] and b1[j] == b1[j-1]:
+                    cache[i+1, j+1] = min(
+                        cache[i-1, i-1] + 1,
+                        cache[i+1, j+1]
+                    )
+
     return cache[m, n]
 
 
@@ -52,5 +69,5 @@ def fast_lsc(s0: str, s1: str, encoding='utf-8') -> int:
 
 
 if __name__ == '__main__':
-    print(fast_edit_distance("abcd", "b"))
+    print(fast_edit_distance("abbcd", "aggcd"))
     print(fast_lsc("tr l", "trs l"))
