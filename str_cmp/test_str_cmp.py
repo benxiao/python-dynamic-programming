1f2056ba0429680188ed14570bca80e49dff5e25
ranxiao <benji.xiao@gmail.com>
2019-12-20 10:28:51 +1100

    renaming of functions for consistency

--- str_cmp/test_str_cmp.py
+++ str_cmp/test_str_cmp.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from jit_str_cmp import fast_edit_distance, fast_lsc
+from str_cmp import fast_edit_distance, fast_lsc_similarity, fast_hamming_distance
+
 
 class EditDistanceTests(TestCase):
     def test_basic_function(self):
@@ -9,6 +10,7 @@ class EditDistanceTests(TestCase):
     def test_repeated_character(self):
         self.assertEqual(fast_edit_distance("abbggd", "abbppd"), 1)
         self.assertEqual(fast_edit_distance("abboggle", "abboddle"), 1)
+        self.assertEqual(fast_edit_distance("fitzgibbon", "fitzbiggon"), 2)
 
     def test_adjacent_character_swap(self):
         self.assertEqual(fast_edit_distance("afdfd", "adffd"), 3)
@@ -18,7 +20,15 @@ class EditDistanceTests(TestCase):
 
 class LongestCommonSequenceTests(TestCase):
     def test_basic_function(self):
-        self.assertEqual(fast_lsc("abc", "abcd"), 3)
+        self.assertEqual(fast_lsc_similarity("abc", "abcd"), 3)
 
     def test_real_world_examples(self):
-        self.assertEqual(fast_lsc("david smith", "david evan smith"), len("david smith"))
+        self.assertEqual(fast_lsc_similarity("david smith", "david evan smith"), len("david smith"))
+
+
+class HammingDistanceTests(TestCase):
+    def test_strings_with_same_length(self):
+        self.assertEqual(fast_hamming_distance("abc", "adc"), 1)
+
+    def test_strings_with_different_length(self):
+        self.assertEqual(fast_hamming_distance("abc", "adcd"), 2)
