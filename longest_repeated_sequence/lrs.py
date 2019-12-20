0fe17c56afa28ca8704a6f94601cfb5186c6b8ed
ranxiao <benji.xiao@gmail.com>
2019-12-20 16:57:27 +1100

    implement the bottom up dp version

--- longest_repeated_sequence/lrs.py
+++ longest_repeated_sequence/lrs.py
@@ -3,9 +3,7 @@ Longest repeated sequence
 
 """
 
-
 seq = "ATACTCGGA"
-
 
 
 def navie_lrs(seq, i, j):
@@ -18,6 +16,18 @@ def navie_lrs(seq, i, j):
     return max(navie_lrs(seq, i-1, j), navie_lrs(seq, i,j-1))
 
 
+def lrs(seq):
+    l = len(seq)
+    cache = [[0] * (l+1) for _ in range(l+1)]
+    for i, c0 in enumerate(seq):
+        for j, c1 in enumerate(seq):
+            if i!=j and c0 == c1:
+                cache[i+1][j+1] = cache[i][j] + 1
+            else:
+                cache[i+1][j+1] = max(cache[i][j+1], cache[i+1][j])
+    return cache[l][l]
+
 
 if __name__ == '__main__':
     print(navie_lrs(seq, len(seq)-1, len(seq)-1))
+    print(lrs(seq))
