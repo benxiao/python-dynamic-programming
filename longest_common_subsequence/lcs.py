b68353f0d3edf3af77318a07e0f9f8cdcf924cac
ranxiao <benji.xiao@gmail.com>
2019-12-20 16:56:59 +1100

    remove some junk code

--- longest_common_subsequence/lcs.py
+++ longest_common_subsequence/lcs.py
@@ -1,4 +1,4 @@
-def lsc(s0, s1):
+def lcs(s0, s1):
     cache = [[0] * (len(s1)+1) for _ in range(len(s0)+1)]
     for i, c0 in enumerate(s0):
         for j, c1 in enumerate(s1):
@@ -12,12 +12,10 @@ def lsc(s0, s1):
     return cache[len(s0)][len(s1)]
 
 
-# if __name__ == '__main__':
-#     # perfect usecase for messy data
-#     print(lsc("trucson le", "truc le"))
-#     print(str_match("trucson le", "truc le"))
-#
-#     # print(lsc("ahmed", "ahamed"))
-#     # print(len("ummuzahira kamaldeen"),
-#     #       len("ummuzahira hanifamohamedkamaldeen"),
-#     #       lsc("ummuzahira kamaldeen", "ummuzahira hanifamohamedkamaldeen"))
+if __name__ == '__main__':
+    # perfect usecase for messy data
+    print(lcs("trucson le", "truc le"))
+    # print(lsc("ahmed", "ahamed"))
+    # print(len("ummuzahira kamaldeen"),
+    #       len("ummuzahira hanifamohamedkamaldeen"),
+    #       lsc("ummuzahira kamaldeen", "ummuzahira hanifamohamedkamaldeen"))
