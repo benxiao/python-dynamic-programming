1fc58af8596b58762556338e1b8d7d93cc3450fd
Ran Xiao <ben.xiao@me.com>
2020-01-26 22:34:20 +1100

    add a header

--- matrix_multiplication/mm.py
+++ matrix_multiplication/mm.py
@@ -1,4 +1,18 @@
+"""
+Input: p[] = {40, 20, 30, 10, 30}
+  Output: 26000
+  There are 4 matrices of dimensions 40x20, 20x30, 30x10 and 10x30.
+  Let the input 4 matrices be A, B, C and D.  The minimum number of
+  multiplications are obtained by putting parenthesis in following way
+  (A(BC))D --> 20*30*10 + 40*20*10 + 40*10*30
 
+  Input: p[] = {10, 20, 30, 40, 30}
+  Output: 30000
+  There are 4 matrices of dimensions 10x20, 20x30, 30x40 and 40x30.
+  Let the input 4 matrices be A, B, C and D.  The minimum number of
+  multiplications are obtained by putting parenthesis in following way
+  ((AB)C)D --> 10*20*30 + 10*30*40 + 10*40*30
+"""
 dimensions = [(40, 20), (20, 30), (30, 10), (10, 30)]
 symbols = ['A', 'B', 'C', 'D']
 dimensions2 = [(10, 20), (20, 30), (30, 40), (40, 30)]
@@ -9,6 +23,7 @@ def opt(demensions, symbols):
         return 0, symbols[0]
     minimum_ops = float('inf')
     minimum_str = ""
+
     for i in range(len(demensions) - 1):
         previous_solution, result_string = opt(
             demensions[:i] + [(demensions[i][0], demensions[i + 1][1])] + demensions[i + 2:],
