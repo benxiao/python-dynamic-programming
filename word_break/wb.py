86ca94aade60a4634ca9423539b2c6f5527b6b8f
rxiao <benji.xiao@gmail.com>
2020-02-02 12:18:31 +1100

    dp add word break

---
+++ word_break/wb.py
@@ -0,0 +1,30 @@
+dictionary = {'I', 'a', 'am', "bad", "ass", "badass"}
+
+
+def word_break(dictionary, text):
+    l = len(text)
+    cache = [[0] * l for _ in range(l)]
+    word_cache = [[None for _ in range(l)] for _ in range(l)]
+
+    for i, c in enumerate(text):
+        cache[i][i] = int(c in dictionary)
+        if cache[i][i]:
+            word_cache[i][i] = [c]
+
+    for step in range(1, l):
+        for start in range(l - step):
+            start, end = start, start + step
+            cache[start][end] = int(text[start: end+1] in dictionary)
+            if cache[start][end]:
+                word_cache[start][end] = [text[start:end+1]]
+            else:
+                for split in range(start+1, end+1):
+                    if cache[start][split-1] and cache[split][end]:
+                        cache[start][end] = 1
+                        word_cache[start][end] = word_cache[start][split-1] + word_cache[split][end]
+
+    return word_cache[0][-1]
+
+
+
+word_break(dictionary, "abadass")
